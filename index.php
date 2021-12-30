9e041159f659a0e211fb0ff9b5ad0885c1ecf445
Omarjetos <93458140+Omarjetos@users.noreply.github.com>
2021-12-30 14:15:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="clckd" content="7e65bb146ad312d31655601449c92e77" />
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
